c96408b8fa426622faa9416380906df519ff3ff2
Glazzerino <fztmon@gmail.com>
2021-05-20 12:50:27 -0500

    RGB

--- catkinws/src/cloudproc/src/cloudproc_node.cpp
+++ catkinws/src/cloudproc/src/cloudproc_node.cpp
@@ -16,9 +16,10 @@
 #include <pcl/visualization/point_cloud_handlers.h>
 #include <pcl/filters/voxel_grid.h>
 #include <vector>
-typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
 
-// pcl::visualization::CloudViewer viewer->"Point Cloud");
+typedef pcl::PointCloud<pcl::PointXYZRGB> PointCloud;
+
+// pcl::visualization::CloudViewer test_viewer("Point Cloud");
 
 boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer(
     new pcl::visualization::PCLVisualizer ("Visualizer")
@@ -30,25 +31,21 @@ pcl::PCLPointCloud2ConstPtr cloudPtr(cloud);
 pcl::PCLPointCloud2* cloud_filtered = new pcl::PCLPointCloud2;
 pcl::PCLPointCloud2Ptr cloud_filtered_ptr (cloud_filtered);
 
-pcl::PointCloud<pcl::PointXYZ>::Ptr temp_cloud(new pcl::PointCloud<pcl::PointXYZ>);
-pcl::PointCloud<pcl::PointXYZ>::Ptr filtered_cloud(new pcl::PointCloud<pcl::PointXYZ>);
-// pcl::SACSegmentation<pcl::PointXYZ> seg;
-// pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
+pcl::PointCloud<pcl::PointXYZRGB>::Ptr temp_cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
+pcl::PointCloud<pcl::PointXYZRGB>::Ptr filtered_cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
+
 
 std::vector<pcl::PointIndices> cluster_indices;
 
-//Were we'll put all clusters
-PointCloud::Ptr final_cloud(new PointCloud);
-
 //Eucledian clustering objclusterert
-pcl::search::KdTree<pcl::PointXYZ>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZ>);
+pcl::search::KdTree<pcl::PointXYZRGB>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZRGB>);
 
-pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_cluster (new pcl::PointCloud<pcl::PointXYZ>);
+pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_cluster (new pcl::PointCloud<pcl::PointXYZRGB>);
 
-pcl::EuclideanClusterExtraction<pcl::PointXYZ> clusterer;
+pcl::EuclideanClusterExtraction<pcl::PointXYZRGB> clusterer;
 
 //Stores individual clusters as point clouds
-std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr> segments;
+std::vector<pcl::PointCloud<pcl::PointXYZRGB>::Ptr> segments;
 
 void callback(const sensor_msgs::PointCloud2ConstPtr& cloud_msg) {
 
@@ -78,9 +75,9 @@ void callback(const sensor_msgs::PointCloud2ConstPtr& cloud_msg) {
     clusterer.extract (cluster_indices);
 
 
-    int j = 0;
+    // int j = 0;
     for (std::vector<pcl::PointIndices>::const_iterator it = cluster_indices.begin (); it != cluster_indices.end (); ++it) {
-        pcl::PointCloud<pcl::PointXYZ>::Ptr individual_cluster (new pcl::PointCloud<pcl::PointXYZ>);
+        pcl::PointCloud<pcl::PointXYZRGB>::Ptr individual_cluster (new pcl::PointCloud<pcl::PointXYZRGB>);
 
         for (std::vector<int>::const_iterator pit = it->indices.begin (); pit != it->indices.end (); ++pit)
             individual_cluster->push_back((*temp_cloud)[*pit]);
@@ -90,17 +87,17 @@ void callback(const sensor_msgs::PointCloud2ConstPtr& cloud_msg) {
         segments.push_back(individual_cluster);
     }
     int counter = 0;
-    for (pcl::PointCloud<pcl::PointXYZ>::Ptr seg : segments) {
+    for (pcl::PointCloud<pcl::PointXYZRGB>::Ptr seg : segments) {
 
         ++counter;
-        pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> color_handler(
+        pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZRGB> color_handler(
             seg,
             rand() % 255,
             rand() % 255,
             rand() % 255);
         std::string id_name = std::to_string(counter);
 
-        viewer->addPointCloud<pcl::PointXYZ> (seg,color_handler ,id_name);
+        viewer->addPointCloud<pcl::PointXYZRGB> (seg,color_handler ,id_name);
          viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, id_name);
     }
     viewer->spinOnce(100);
@@ -113,7 +110,7 @@ int main(int argc, char** argv) {
 
     ros::init(argc, argv, "point_cloud_processing");
     ros::NodeHandle handler;
-    ros::Subscriber subscriber = handler.subscribe("points", 10, callback);
+    ros::Subscriber subscriber = handler.subscribe("frontr200/camera/depth_registered/points", 10, callback);
 
     ros::spin();
     // while(!viewer->wasStopped()) {
