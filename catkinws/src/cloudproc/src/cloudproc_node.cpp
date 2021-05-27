9188fbe74f6ebd06fee7bbfb25e395d1fac922d7
Glazzerino <fztmon@gmail.com>
2021-05-27 13:06:00 -0500

    gate detection (v1)

--- catkinws/src/cloudproc/src/cloudproc_node.cpp
+++ catkinws/src/cloudproc/src/cloudproc_node.cpp
@@ -6,6 +6,7 @@
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/visualization/cloud_viewer.h>
 #include <pcl/visualization/pcl_visualizer.h>
+#include <pcl/common/common.h>
 #include <pcl/sample_consensus/method_types.h>
 #include <pcl/sample_consensus/model_types.h>
 #include <pcl/segmentation/sac_segmentation.h>
@@ -35,7 +36,6 @@ pcl::PCLPointCloud2Ptr cloud_filtered_ptr (cloud_filtered);
 pcl::PointCloud<pcl::PointXYZRGB>::Ptr temp_cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
 pcl::PointCloud<pcl::PointXYZRGB>::Ptr filtered_cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
 
-
 std::vector<pcl::PointIndices> cluster_indices;
 
 //Eucledian clustering objclusterert
@@ -45,7 +45,6 @@ pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_cluster (new pcl::PointCloud<pcl::P
 
 pcl::EuclideanClusterExtraction<pcl::PointXYZRGB> clusterer;
 
-float test_input = 0;
 
 //Stores individual clusters as point clouds
 std::vector<pcl::PointCloud<pcl::PointXYZRGB>::Ptr> segments;
@@ -60,13 +59,13 @@ void callback(const sensor_msgs::PointCloud2ConstPtr& cloud_msg) {
 
     pcl_conversions::toPCL(*cloud_msg, *cloud);
 
-    clusterer.setClusterTolerance (0.02); // 2cm
+    clusterer.setClusterTolerance (0.2); // 20cm
     clusterer.setMinClusterSize (100);
     clusterer.setMaxClusterSize (25000);
 
     pcl::VoxelGrid<pcl::PCLPointCloud2> voxel_grid;
     voxel_grid.setInputCloud(cloudPtr);
-    voxel_grid.setLeafSize(0.01,0.01, 0.01);
+    voxel_grid.setLeafSize(0.05,0.05, 0.05);
     voxel_grid.filter(*cloud_filtered_ptr);
 
     std::vector<int> indices;
@@ -75,17 +74,15 @@ void callback(const sensor_msgs::PointCloud2ConstPtr& cloud_msg) {
     pcl::PassThrough<pcl::PointXYZRGB> pass;
     pass.setInputCloud(temp_cloud);
     pass.setFilterFieldName("y");
-    pass.setFilterLimits(-test_input, FLT_MAX);
+    pass.setFilterLimits(-3.25, FLT_MAX); // -3.25m to inf height
     pass.filter(*filtered_cloud);
 
     pcl::removeNaNFromPointCloud(*filtered_cloud, *filtered_cloud, indices);
 
-    clusterer.setSearchMethod (tree);
-    clusterer.setInputCloud (filtered_cloud);
-    clusterer.extract (cluster_indices);
+    clusterer.setSearchMethod(tree);
+    clusterer.setInputCloud(filtered_cloud);
+    clusterer.extract(cluster_indices);
 
-
-    // int j = 0;
     for (std::vector<pcl::PointIndices>::const_iterator it = cluster_indices.begin (); it != cluster_indices.end (); ++it) {
         pcl::PointCloud<pcl::PointXYZRGB>::Ptr individual_cluster (new pcl::PointCloud<pcl::PointXYZRGB>);
 
@@ -97,26 +94,58 @@ void callback(const sensor_msgs::PointCloud2ConstPtr& cloud_msg) {
         segments.push_back(individual_cluster);
     }
     int counter = 0;
+
+    pcl::PointXYZRGB min_p, max_p, mid_p, gate_mid_p;
+
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr gate;
+    float max_diagonal = 0;
     for (pcl::PointCloud<pcl::PointXYZRGB>::Ptr seg : segments) {
+        pcl::getMinMax3D(*seg, min_p, max_p);
+        mid_p.x = (min_p.x + max_p.x) / 2;
 
+        mid_p.y = (min_p.y + max_p.y) / 2;
+
+        mid_p.z = (min_p.z + max_p.z) / 2;
+        float diagonal = std::sqrt(std::pow(max_p.x - min_p.x, 2) + std::pow(max_p.y - min_p.y, 2) + std::pow(max_p.z - min_p.z, 2));
+        if (diagonal > max_diagonal) {
+            max_diagonal = diagonal;
+            gate = seg;
+            gate_mid_p = mid_p;
+        } else {
+            pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZRGB> color_handler(
+                seg,
+                255,
+                0,
+                0);
+            std::string id_name = std::to_string(counter);
+
+            viewer->addPointCloud<pcl::PointXYZRGB> (seg,color_handler ,id_name);
+            viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, id_name);
+
+        }
         ++counter;
-        pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZRGB> color_handler(
-            seg,
-            rand() % 255,
-            rand() % 255,
-            rand() % 255);
-        std::string id_name = std::to_string(counter);
+    }
+    std::cout << gate << "\n";
 
-        viewer->addPointCloud<pcl::PointXYZRGB> (seg,color_handler ,id_name);
-         viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, id_name);
+    if (gate != nullptr) {
+        std::cout << "gate found\n";
+        viewer->addSphere(gate_mid_p, 0.05);
+        // std::cout << "Min: " << min_p << "Max: " << max_p << "\n";
+        pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZRGB> color_handler(
+            gate,
+            0,
+            255,
+            111);
+        std::string id_name = "Gate";
+
+        viewer->addPointCloud<pcl::PointXYZRGB> (gate, color_handler ,id_name);
+        viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, id_name);
     }
     viewer->spinOnce(100);
 }
 
 int main(int argc, char** argv) {
 
-    std::cout << "Enter max height: ";
-    std::cin >> test_input;
     viewer->setBackgroundColor(0,0,0);
     segments.reserve(10);
 
@@ -125,8 +154,4 @@ int main(int argc, char** argv) {
     ros::Subscriber subscriber = handler.subscribe("frontr200/camera/depth_registered/points", 10, callback);
 
     ros::spin();
-    // while(!viewer->wasStopped()) {
-    //     ros::spinOnce();
-    //     viewer->spinOnce(20, true);
-    // }
 }
